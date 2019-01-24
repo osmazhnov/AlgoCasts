3c0ecc40301903fcff66aab27d0c66d545a0105c
Oleh <oleg_smagnov@mail.ru>
2019-01-24 10:35:14 +0200

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const reduceHandler = (obj, char) => {
+        if(obj[char]){
+            obj[char] += 1;
+        } else {
+            obj[char] = 1;
+        }
+        return obj;
+    };
+    const object = str.split('').reduce(reduceHandler, {});
+    let max = str[0];
+    const forEachHandler = (key) => {
+        if(object[key] > object[max]){
+            max = key;
+        }
+    };
+    Object.keys(object).forEach(forEachHandler);
+
+    return max;
+}
 
 module.exports = maxChar;
